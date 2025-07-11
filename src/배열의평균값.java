68ffe05e48434241120aa440cd054902399e55c0
Johs <206869862+desembers@users.noreply.github.com>
2025-07-11 10:02:19 +0900

    ãcode-kata2

---
+++ src/배열의평균값.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Solution7 {
+    public double solution(int[] numbers) {
+        int sum=0;
+        for(int num : numbers) {
+            sum += num;
+        }
+        return (double) sum / numbers.length;
+
+    }
+}
+
+public class 배열의평균값 {
+    public static void main(String[] args) {
+        Solution7 sol = new Solution7();
+        System.out.println(sol.solution(new int[]{1,2,3,4,5,6,7,8,9,10}));
+    }
+}
