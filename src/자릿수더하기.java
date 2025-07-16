f710a5a8aac12a65a50d8906ce1e1504db97a33f
Johs <206869862+desembers@users.noreply.github.com>
2025-07-16 11:26:38 +0900

    code-kata3

--- src/자릿수더하기.java
+++ src/자릿수더하기.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.*;
+class Solution10 {
+    public int solution(int n) {
+        int sum = 0;
+        while(n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
 
 public class 자릿수더하기 {
+    public static void main(String[] args) {
+        Solution10 sol = new Solution10();
+        System.out.println(sol.solution(123));
+        System.out.println(sol.solution(987));
+    }
 }
