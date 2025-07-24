d86e993bf703854d26a7f058bf68a55b24621141
desembers <toastpcyco4786@gmail.com>
2025-07-24 23:17:22 +0900

    Update 콜라츠추측.java

--- src/콜라츠추측.java
+++ src/콜라츠추측.java
@@ -2,10 +2,8 @@ class Solution20 {
     public int solution(long num) {
         int count = 0;
 
-        // 1인 경우는 0을 반환
         if (num == 1) return 0;
 
-        // 최대 500번 반복
         while (num != 1 && count < 500) {
             if (num % 2 == 0) {
                 num /= 2;
@@ -15,7 +13,6 @@ class Solution20 {
             count++;
         }
 
-        // 500번 안에 1이 되면 횟수 반환, 아니면 -1
         return num == 1 ? count : -1;
     }
 }
